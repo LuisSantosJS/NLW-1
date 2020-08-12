71a29ad7309ccbfcdcbfc5b68f6bdd5c4b1cbed2
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-08-12 07:57:41 -0500

    upload

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express, { request } from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
+import { errors } from 'celebrate';
+
+const app = express();
+
+app.use(cors());
+
+// Adicionando uma funcionalidade no express por meio do 'use'
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
+
+app.listen(3333);
